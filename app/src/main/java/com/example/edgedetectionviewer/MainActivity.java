8510d1a8511f03bf617cd5484449f177f5233dde
Samarth B Y <samarthgowda453@gmail.com>
2025-11-13 17:21:23 +0530

    added camera2 preview using TextureView working successfully

--- app/src/main/java/com/example/edgedetectionviewer/MainActivity.java
+++ app/src/main/java/com/example/edgedetectionviewer/MainActivity.java
@@ -1,26 +1,126 @@
 package com.example.edgedetectionviewer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.*;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.TextureView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
+
     static {
         System.loadLibrary("edgedetectionviewer");
     }
+
+    private TextureView textureView;
+    private CameraDevice cameraDevice;
+    private CameraCaptureSession cameraCaptureSession;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        textureView = findViewById(R.id.textureView);
+
+        textureView.setSurfaceTextureListener(surfaceTextureListener);
+    }
+
+    private final TextureView.SurfaceTextureListener surfaceTextureListener =
+            new TextureView.SurfaceTextureListener() {
+                @Override
+                public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
+                    openCamera();
+                }
+
+                @Override
+                public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surface, int width, int height) {}
+
+                @Override
+                public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surface) {
+                    return false;
+                }
+
+                @Override
+                public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surface) {}
+            };
+
+    private void openCamera() {
+        CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+
+        try {
+            String cameraId = manager.getCameraIdList()[0];
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA}, 100);
+                return;
+            }
+
+            manager.openCamera(cameraId, stateCallback, null);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private final CameraDevice.StateCallback stateCallback =
+            new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(@NonNull CameraDevice camera) {
+                    cameraDevice = camera;
+                    startCameraPreview();
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice camera) {}
+
+                @Override
+                public void onError(@NonNull CameraDevice camera, int error) {}
+            };
+
+    private void startCameraPreview() {
+        SurfaceTexture surfaceTexture = textureView.getSurfaceTexture();
+        surfaceTexture.setDefaultBufferSize(1920, 1080);
+
+        Surface surface = new Surface(surfaceTexture);
+
+        try {
+            CaptureRequest.Builder builder =
+                    cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+
+            builder.addTarget(surface);
+
+            cameraDevice.createCaptureSession(
+                    java.util.Collections.singletonList(surface),
+                    new CameraCaptureSession.StateCallback() {
+                        @Override
+                        public void onConfigured(@NonNull CameraCaptureSession session) {
+                            cameraCaptureSession = session;
+
+                            try {
+                                session.setRepeatingRequest(builder.build(), null, null);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {}
+                    },
+                    null
+            );
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
